567146495d48397a4ac8dd3c8d8dc3a98b37fe32
Amir Rajan <ar@amirrajan.net>
2015-05-18 22:20:31 -0500

    make it so

---
+++ bouncing-balls.clj
@@ -0,0 +1,48 @@
+;;http://www.codewars.com/kata/5544c7a5cb454edb3c000047/train/clojure
+;;
+;;A child plays with a ball on the nth floor of a big building the height of which is known
+;;
+;;(float parameter "h" in meters, h > 0) .
+;;
+;;He lets out the ball. The ball rebounds for example to two-thirds
+;;
+;;(float parameter "bounce", 0 < bounce < 1)
+;;
+;;of its height.
+;;
+;;His mother looks out of a window that is 1.5 meters from the ground
+;;
+;;(float parameters window < h).
+;;
+;;How many times will the mother see the ball either falling or bouncing in front of the window
+;;
+;;(return a positive integer unless conditions are not fulfilled in which case return -1) ?
+;;
+;;Note
+;;
+;;You will admit that the ball can only be seen if the height of the rebouncing ball is stricty greater than the window parameter.
+;;
+;;Example:
+;;
+;;h = 3, bounce = 0.66, window = 1.5, result is 3
+;;
+;;h = 3, bounce = 1, window = 1.5, result is -1
+
+(ns bouncing-balls.core)
+
+(defn bouncing-balls [h bounce window]
+  ; your code
+)
+
+(ns bouncing-balls.core-test
+  (:require [clojure.test :refer :all]
+            [bouncing-balls.core :refer :all]))
+
+(deftest a-test1
+  (testing "Test 1"
+    (def rr 3)
+    (is (= (bouncing-balls 3 0.66 1.5) rr))))
+(deftest a-test2
+  (testing "Test 2"
+    (def rr 15)
+        (is (= (bouncing-balls 30 0.66 1.5) rr))))
