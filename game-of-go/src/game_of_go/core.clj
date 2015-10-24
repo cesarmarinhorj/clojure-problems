f4386e30ad146806c93381edbd8847688724c251
Amir Rajan <ar@amirrajan.net>
2015-10-23 20:47:32 -0500

    simplification of stuff and eye logic

--- game-of-go/src/game_of_go/core.clj
+++ game-of-go/src/game_of_go/core.clj
@@ -16,13 +16,21 @@
 (defn next-turn [current-turn]
   (if (= current-turn :w) :b :w))
 
-(defn move [board row-pos col-pos turn]
-  {:board (if (empty-at? board row-pos col-pos)
-            (assoc-in board [row-pos col-pos] turn)
-            board)
-   :turn (next-turn turn)})
+(defn eye? [board row-pos col-pos turn]
+  (let [above (get-in board [(dec row-pos) col-pos])
+        below (get-in board [(inc row-pos) col-pos])
+        left (get-in board [row-pos (dec col-pos)])
+        right (get-in board [row-pos (inc col-pos)])]
+    (= above below left right (next-turn turn))))
 
-(deftest placement
+
+(defn move [board row-pos col-pos turn]
+  (if (and (empty-at? board row-pos col-pos) (not (eye? board row-pos col-pos turn)))
+    {:board (assoc-in board [row-pos col-pos] turn)
+     :turn (next-turn turn)}
+    {:board board :turn turn}))
+
+(deftest hello-world
   (testing "simple placement"
     (is (= (:board (move [[:. :. :. :. :.]
                           [:. :. :. :. :.]
@@ -48,6 +56,30 @@
             [:. :. :. :. :.]
             [:. :. :. :. :.]
             [:. :. :. :. :.]
-            [:. :. :. :. :.]]))))
+            [:. :. :. :. :.]])))
+
+  (testing "turn remains unchanged if invalid placement"
+    (is (= (:turn (move [[:b :. :. :.]] 0 0 :w)) :w))))
+
+(deftest eyes
+  (testing "can't place in an eye of the opposite color"
+    (is (= (move [[:. :. :b :. :.]
+                  [:. :b :. :b :.]
+                  [:. :. :b :. :.]] 1 2 :w)
+
+           {:board [[:. :. :b :. :.]
+                    [:. :b :. :b :.]
+                    [:. :. :b :. :.]]
+            :turn :w})))
+
+  (testing "can place in an eye of the same color"
+    (is (= (move [[:. :. :b :. :.]
+                  [:. :b :. :b :.]
+                  [:. :. :b :. :.]] 1 2 :b)
+
+           {:board [[:. :. :b :. :.]
+                    [:. :b :b :b :.]
+                    [:. :. :b :. :.]]
+            :turn :w}))))
 
 (run-tests)
