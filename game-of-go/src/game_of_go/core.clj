6e894dad683f9605d0287105a87320b9d94d21e5
Amir Rajan <ar@amirrajan.net>
2015-10-29 21:53:06 -0500

    moved go tests to seperate file

--- game-of-go/src/game_of_go/core.clj
+++ game-of-go/src/game_of_go/core.clj
@@ -1,7 +1,6 @@
 (ns game-of-go.core
   (:require [clojure.repl :as repl]
             [clojure.data.json :as json]
-            [clojure.test :refer :all]
             [clojure.java.io :as io]))
 
 (def five-by-five [[:. :. :. :. :.]
@@ -57,96 +56,3 @@
   (if (= col-pos -1)
     nil
     [row-pos col-pos]))
-
-(defn future-move [board]
-  (->> board
-       (map-indexed #(to-row-col-pos % (.indexOf %2 :B)))
-       (remove nil?)
-       (first)))
-
-(defn verify-board [from to]
-  (future-move from))
-
-(defn verify-board [board]
-  (let [[row-pos col-pos] (future-move board)]
-    [row-pos col-pos]))
-
-(deftest hello-world
-  (testing "simple placement"
-    (is (= (:board (move [[:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]] 0 0 :w))
-
-           [[:w :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]])))
-
-  (testing "can't place at a location that already has a piece"
-    (is (= (:board (move
-                    [[:w :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]] 0 0 :b))
-
-           [[:w :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]])))
-
-  (testing "turn remains unchanged if invalid placement"
-    (is (= (:turn (move [[:b :. :. :.]] 0 0 :w)) :w))))
-
-(deftest eyes
-  (testing "can't place in an eye of the opposite color"
-    (is (= (move [[:. :. :b :. :.]
-                  [:. :b :. :b :.]
-                  [:. :. :b :. :.]] 1 2 :w)
-
-           {:board [[:. :. :b :. :.]
-                    [:. :b :. :b :.]
-                    [:. :. :b :. :.]]
-            :turn :w})))
-
-  (testing "can place in an eye of the same color"
-    (is (= (move [[:. :. :b :. :.]
-                  [:. :b :. :b :.]
-                  [:. :. :b :. :.]] 1 2 :b)
-
-           {:board [[:. :. :b :. :.]
-                    [:. :b :b :b :.]
-                    [:. :. :b :. :.]]
-            :turn :w}))))
-
-(deftest captures
-  (testing "simple capture"
-    (is (= (move [[:. :. :w :. :.]
-                  [:. :w :b :. :.]
-                  [:. :. :w :. :.]] 1 3 :w)
-
-           {:board [[:. :. :w :. :.]
-                    [:. :w :. :w :.]
-                    [:. :. :w :. :.]]
-            :turn :b}))))
-
-(run-tests)
-
-;; (verify-board [[:. :. :b :. :.]
-;;                [:. :b :w :B :.]
-;;                [:. :. :b :. :.]])
-
-;; (defn verify-board [from to])
-
-;; (verify-board [[:. :. :b :. :.]
-;;                [:. :b :w :B :.]
-;;                [:. :. :b :. :.]]
-
-
-;;               [[:. :. :b :. :.]
-;;                [:. :b :. :b :.]
-;;                [:. :. :b :. :.]])
--- game-of-go/test/game_of_go/core_test.clj
+++ game-of-go/test/game_of_go/core_test.clj
@@ -2,6 +2,86 @@
   (:require [clojure.test :refer :all]
             [game-of-go.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn future-move [board future-turn]
+  (->> board
+       (map-indexed #(to-row-col-pos % (.indexOf %2 future-turn)))
+       (remove nil?)
+       (first)))
+
+(defn verify-board [board]
+  (let [[row-pos col-pos] (or (future-move board :B) (future-move board :W))]
+    [row-pos col-pos]))
+
+(deftest hello-world
+  (testing "simple placement"
+    (is (= (:board (move [[:. :. :. :. :.]
+                          [:. :. :. :. :.]
+                          [:. :. :. :. :.]
+                          [:. :. :. :. :.]
+                          [:. :. :. :. :.]] 0 0 :w))
+
+           [[:w :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]])))
+
+  (testing "can't place at a location that already has a piece"
+    (is (= (:board (move
+                    [[:w :. :. :. :.]
+                     [:. :. :. :. :.]
+                     [:. :. :. :. :.]
+                     [:. :. :. :. :.]
+                     [:. :. :. :. :.]] 0 0 :b))
+
+           [[:w :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]
+            [:. :. :. :. :.]])))
+
+  (testing "turn remains unchanged if invalid placement"
+    (is (= (:turn (move [[:b :. :. :.]] 0 0 :w)) :w))))
+
+(deftest eyes
+  (testing "can't place in an eye of the opposite color"
+    (is (= (move [[:. :. :b :. :.]
+                  [:. :b :. :b :.]
+                  [:. :. :b :. :.]] 1 2 :w)
+
+           {:board [[:. :. :b :. :.]
+                    [:. :b :. :b :.]
+                    [:. :. :b :. :.]]
+            :turn :w})))
+
+  (testing "can place in an eye of the same color"
+    (is (= (move [[:. :. :b :. :.]
+                  [:. :b :. :b :.]
+                  [:. :. :b :. :.]] 1 2 :b)
+
+           {:board [[:. :. :b :. :.]
+                    [:. :b :b :b :.]
+                    [:. :. :b :. :.]]
+            :turn :w}))))
+
+(deftest captures
+  (testing "simple capture"
+    (is (= (move [[:. :. :w :. :.]
+                  [:. :w :b :. :.]
+                  [:. :. :w :. :.]] 1 3 :w)
+
+           {:board [[:. :. :w :. :.]
+                    [:. :w :. :w :.]
+                    [:. :. :w :. :.]]
+            :turn :b}))))
+
+(verify-board [[:. :. :b :. :.]
+               [:. :b :w :B :.]
+               [:. :. :b :. :.]])
+
+
+(run-tests)
+
+(future-move [[:. :. :b :. :.]
+              [:. :b :w :B :.]
+              [:. :. :b :. :.]] :B)
