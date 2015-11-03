b5728dd0abe4c24e9fd3e0a51dece1052685a0ef
Amir Rajan <ar@amirrajan.net>
2015-11-02 22:05:12 -0600

    cleaned up tests to use `verify-board`

--- game-of-go/test/game_of_go/core_test.clj
+++ game-of-go/test/game_of_go/core_test.clj
@@ -8,80 +8,74 @@
        (remove nil?)
        (first)))
 
-(defn verify-board [board]
-  (let [[row-pos col-pos] (or (future-move board :B) (future-move board :W))]
-    [row-pos col-pos]))
+(defn verify-board [board-before board-after turn-after]
+  (let [[b-row-pos b-col-pos :as future-b-pos] (future-move board-before :B)
+        [w-row-pos w-col-pos :as future-w-pos] (future-move board-before :W)
+        board (remove-future-moves board-before)
+        expected {:board board-after :turn turn-after }]
+    (cond future-b-pos (is (= (move board b-row-pos b-col-pos :b) expected))
+          future-w-pos (is (= (move board w-row-pos w-col-pos :w) expected))
+          :else nil)))
+
+(defn remove-future-moves [board]
+  (into [] (map #(replace {:W :. :B :.} %) board)))
 
 (deftest hello-world
   (testing "simple placement"
-    (is (= (:board (move [[:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]
-                          [:. :. :. :. :.]] 0 0 :w))
+    (verify-board [[:W :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]]
 
-           [[:w :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]])))
+                  [[:w :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]
+                   [:. :. :. :. :.]] :b))
 
   (testing "can't place at a location that already has a piece"
-    (is (= (:board (move
-                    [[:w :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]
-                     [:. :. :. :. :.]] 0 0 :b))
+    (is (= (move [[:w :. :. :. :.]
+                  [:. :. :. :. :.]
+                  [:. :. :. :. :.]
+                  [:. :. :. :. :.]
+                  [:. :. :. :. :.]] 0 0 :b)
 
-           [[:w :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]
-            [:. :. :. :. :.]])))
-
-  (testing "turn remains unchanged if invalid placement"
-    (is (= (:turn (move [[:b :. :. :.]] 0 0 :w)) :w))))
+           {:board [[:w :. :. :. :.]
+                    [:. :. :. :. :.]
+                    [:. :. :. :. :.]
+                    [:. :. :. :. :.]
+                    [:. :. :. :. :.]]
+            :turn :b}))))
 
 (deftest eyes
   (testing "can't place in an eye of the opposite color"
-    (is (= (move [[:. :. :b :. :.]
-                  [:. :b :. :b :.]
-                  [:. :. :b :. :.]] 1 2 :w)
+    (verify-board [[:. :. :b :. :.]
+                   [:. :b :W :b :.]
+                   [:. :. :b :. :.]]
 
-           {:board [[:. :. :b :. :.]
-                    [:. :b :. :b :.]
-                    [:. :. :b :. :.]]
-            :turn :w})))
+                  [[:. :. :b :. :.]
+                   [:. :b :. :b :.]
+                   [:. :. :b :. :.]] :w))
 
-  (testing "can place in an eye of the same color"
-    (is (= (move [[:. :. :b :. :.]
-                  [:. :b :. :b :.]
-                  [:. :. :b :. :.]] 1 2 :b)
+  (testing "placement in an eye of the same color is allowed"
+    (verify-board [[:. :. :b :. :.]
+                   [:. :b :B :b :.]
+                   [:. :. :b :. :.]]
 
-           {:board [[:. :. :b :. :.]
-                    [:. :b :b :b :.]
-                    [:. :. :b :. :.]]
-            :turn :w}))))
+                  [[:. :. :b :. :.]
+                   [:. :b :b :b :.]
+                   [:. :. :b :. :.]] :w)))
 
 (deftest captures
   (testing "simple capture"
-    (is (= (move [[:. :. :w :. :.]
-                  [:. :w :b :. :.]
-                  [:. :. :w :. :.]] 1 3 :w)
+    (verify-board [[:. :. :w :. :.]
+                   [:. :w :b :W :.]
+                   [:. :. :w :. :.]]
 
-           {:board [[:. :. :w :. :.]
-                    [:. :w :. :w :.]
-                    [:. :. :w :. :.]]
-            :turn :b}))))
-
-(verify-board [[:. :. :b :. :.]
-               [:. :b :w :B :.]
-               [:. :. :b :. :.]])
+                  [[:. :. :w :. :.]
+                   [:. :w :. :w :.]
+                   [:. :. :w :. :.]] :b)))
 
 
 (run-tests)
-
-(future-move [[:. :. :b :. :.]
-              [:. :b :w :B :.]
-              [:. :. :b :. :.]] :B)
